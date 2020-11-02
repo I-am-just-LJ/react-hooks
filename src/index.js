f9328dc61ec53039165b95950992e52f8c223876
LJ <jhlee9193@gmail.com>
2020-11-02 16:31:55 +0900

    useState

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import "./styles.css";
 
 const App = () => {
   const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
   return (
     <div className="App">
-      <h1>React Hooks!</h1>
+      <h1>React Hooks! {item}</h1>
       <h2>Start coding</h2>
+      <button onClick={incrementItem}>increment</button>
+      <button onClick={decrementItem}>Decrement</button>
     </div>
   );
 };
