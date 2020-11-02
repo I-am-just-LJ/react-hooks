6498d4427604b430d9a85195dd612b1ff483a7a2
LJ <jhlee9193@gmail.com>
2020-11-02 17:54:53 +0900

    useInput

--- src/index.js
+++ src/index.js
@@ -3,16 +3,40 @@ import ReactDom from "react-dom";
 
 import "./styles.css";
 
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
+
 const App = () => {
   const [item, setItem] = useState(1);
   const incrementItem = () => setItem(item + 1);
   const decrementItem = () => setItem(item - 1);
+
+  const maxLen = (value) => !value.includes("@");
+  // const maxLen = (value) => value.length < 10;
+  const name = useInput("Mr.", maxLen);
   return (
     <div className="App">
-      <h1>React Hooks! {item}</h1>
-      <h2>Start coding</h2>
+      <h1>Learning React Hooks</h1>
+      <h2>useState {item}</h2>
       <button onClick={incrementItem}>increment</button>
       <button onClick={decrementItem}>Decrement</button>
+      <h2>useInput</h2>
+      <input placeholder="Name" {...name} />
+      {/* or value={name.value} */}
     </div>
   );
 };
